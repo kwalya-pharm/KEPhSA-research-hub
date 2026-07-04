4597fe4eea2674a9dc1f74b59b248a18d7075cd3
Morgan Kwalya <kwalyamorgan042@gmail.com>
2026-07-04 19:36:03 +0300

    Add new reports for 2024 and 2025 in PDF format

---
+++ home.js
@@ -0,0 +1,31 @@
+VANTA.NET({
+
+    el:"#hero",
+
+    mouseControls:true,
+
+    touchControls:true,
+
+    gyroControls:false,
+
+    minHeight:200,
+
+    minWidth:200,
+
+    scale:1,
+
+    scaleMobile:1,
+
+    color:0x1e88e5,
+
+    backgroundColor:0x031427,
+
+    points:12,
+
+    maxDistance:22,
+
+    spacing:18,
+
+    showDots:true
+
+});
